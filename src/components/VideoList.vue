7d1a98d3b6ddf758a26d51135fc27e2a40babd05
Saad Butt <khawajamuhammadsaad.butt@glasgow.ac.uk>
2023-09-04 16:19:59 +0100

    fixing the retrieving the data of the users

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,43 +1,42 @@
 <template>
-    <div>
-      <h2>Video Library</h2>
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <ul>
-          <li v-for="video in videos" :key="video._id">
-            {{ video.filename }}
-          </li>
-        </ul>
-        <div v-if="videos.length === 0">No videos found.</div>
-      </div>
+  <div>
+    <h2>Video Library</h2>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="(video, index) in videos" :key="index">
+          <a :href="video" target="_blank">{{ video }}</a>
+        </li>
+      </ul>
+      <div v-if="videos.length === 0">No videos found.</div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        videos: [],
-        loading: true,
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videos: [],
+      loading: true,
+    };
+  },
+  mounted() {
+    this.fetchVideos();
+  },
+  methods: {
+    async fetchVideos() {
+      try {
+        const response = await fetch('https://rich-cyan-termite-hose.cyclic.app/api/retrieve');
+        const data = await response.json();
+        this.videos = data.videoUrls;
+        console.log(this.videos);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      } finally {
+        this.loading = false;
+      }
     },
-    mounted() {
-      this.fetchVideos();
-    },
-    methods: {
-      async fetchVideos() {
-        try {
-          const response = await fetch('https://rich-cyan-termite-hose.cyclic.app/api/retrieve');
-          const data = await response.json();
-          this.videos = data;
-          console.log(this.videos)
-          console.log(data)
-        } catch (error) {
-          console.error('Error fetching videos:', error);
-        } finally {
-          this.loading = false;
-        }
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
